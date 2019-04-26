7ff21935677652ce938f467042cbed18f67e8e8b
Arseny Pronin <arseny707@mail.ru>
2019-04-27 00:48:47 +0300

    completed get_time handler

--- bot_handler/response.py
+++ bot_handler/response.py
@@ -4,6 +4,7 @@ import re
 from datetime import datetime
 from telegram_calendar_keyboard import calendar_keyboard
 from telegram import ReplyKeyboardRemove
+from telegram import ForceReply
 
 
 DEF_TZ = pytz.timezone('Europe/Moscow')
@@ -60,36 +61,44 @@ def update_deadline(update, context):
     handler = db_connector.DataBaseConnector()
     chat_id = update.message.chat.id
     user_id = update.message.from_user.id
-    # remove leading command
-    task_id = re.sub('/dl ', '', update.message.text, 1)
 
-    # todo: Get real datetime
-    update.message.reply_text(f'Пожалуйста, выберите дату для задания {task_id}', reply_markup=calendar_keyboard.create_calendar())    
+    try:
+        task_id = re.sub('/dl ', '', update.message.text, 1)
+        due_date = datetime(2019, 5, 30, 12, 30, 0)
+        due_date = DEF_TZ.localize(due_date)
+        success = handler.set_deadline(task_id, chat_id, user_id, due_date)
+    except (ValueError, ConnectionError):
+        update.message.reply_text('Извините, не получилось.')
+        return
+    if not success:
+        update.message.reply_text('Вы не можете установить срок этому заданию.',
+                                  disable_notification=True)
+    else:
+        update.message.reply_text(f'Пожалуйста, выберите дату для задания {task_id}',
+                                  reply_markup=calendar_keyboard.create_calendar())
 
-
-def inline_handler(update, context):
-    selected, date, update.message = calendar_keyboard.process_calendar_selection(update, context)
-
+def inline_calendar_handler(update, context):
+    selected, full_date, update.message = calendar_keyboard.process_calendar_selection(update, context)
+    
     if selected:
-        update.message.reply_text(f'Вы выбрали {date.strftime("%d/%m/%Y")}\n', reply_markup=ReplyKeyboardRemove())
-
+        update.message.reply_text(f'Вы выбрали {full_date.strftime("%d/%m/%Y")}\n',
+                                  reply_markup=ReplyKeyboardRemove())
     
     handler = db_connector.DataBaseConnector()
-    # 563114293 
     update.message.bot.delete_message(update.message.chat.id, update.message.message_id)
     
     task_id = re.sub('Пожалуйста, выберите дату для задания ', '', update.message.text, 1)
     chat_id = update.message.chat.id
-    user_id = update._effective_user.id #need to be fixed
+    user_id = update.callback_query.from_user.id #update._effective_user.id #need to be fixed
     
-    year = int(date.strftime("%Y"))
-    month = int(date.strftime("%m"))
-    date = int(date.strftime("%d"))
+    year = int(full_date.strftime("%Y"))
+    month = int(full_date.strftime("%m"))
+    date = int(full_date.strftime("%d"))
     due_date = datetime(year, month, date, 12, 0, 0)
     due_date = DEF_TZ.localize(due_date)
 
     try:
-        success = handler.set_deadline(29, chat_id, user_id, due_date)
+        success = handler.set_deadline(task_id, chat_id, user_id, due_date)
     except (ValueError, ConnectionError):
         update.message.reply_text('Извините, не получилось.')
         return
@@ -97,34 +106,49 @@ def inline_handler(update, context):
         update.message.reply_text('Вы не можете установить срок этому заданию.')
     else:
         update.message.reply_text('Срок выполнения установлен.')
+
+    update.message.reply_text(f'Вы выбрали {full_date.strftime("%d/%m/%Y")}\n' +
+                              f'Введите /time \'время дедлайна\'(hh:mm:ss) ' +
+                              f'для задачи {task_id}', reply_markup=ForceReply())
+
 
 def get_time(update, context):
-    handler = db_connector.DataBaseConnector()
-    chat_id = update.message.chat.id
-    user_id = update.message.from_user.id
-    # remove leading command
-    time = re.sub('/time ', '', update.message.text, 1)
+    if (update.message.reply_to_message.text.find('Введите /time \'время дедлайна\'(hh:mm:ss) для задачи ') != -1):
+        handler = db_connector.DataBaseConnector()
+        chat_id = update.message.chat.id
+        user_id = update.message.from_user.id
+        # remove leading command
+        time = re.sub('/time ', '', update.message.text, 1)
+        task_id = update.message.reply_to_message.text[update.message.reply_to_message.text.find('\n') + 52:]
 
-    hours = time[:time.find(':')].strip()
-    minutes = time[time.find(':') + 1:time.find(':', time.find(':') + 1)].strip()
-    seconds = time[time.find(':', time.find(':') + 1) + 1:].strip()
-
-    if (hours.isdigit() and minutes.isdigit() and seconds.isdigit()):
-        due_date = datetime(2018, 5, 30, int(hours), int(minutes), int(seconds))
-        due_date = DEF_TZ.localize(due_date)
-    else:
-        due_date = datetime(2018, 5, 30, 12, 0, 0)
-        due_date = DEF_TZ.localize(due_date)
+        date = int(update.message.reply_to_message.text[11:13])
+        month = int(update.message.reply_to_message.text[14:16])
+        year = int(update.message.reply_to_message.text[17:21])
         
-    try:
-        success = handler.set_deadline(29, chat_id, user_id, due_date)
-    except (ValueError, ConnectionError):
-        update.message.reply_text('Извините, не получилось.')
-        return
-    if not success:
-        update.message.reply_text('Вы не можете установить срок этому заданию.')
+        hours = time[:time.find(':')].strip()
+        minutes = time[time.find(':') + 1:time.find(':', time.find(':') + 1)].strip()
+        seconds = time[time.find(':', time.find(':') + 1) + 1:].strip()
+
+        try:
+            if (hours.isdigit() and minutes.isdigit() and seconds.isdigit()):
+                due_date = datetime(year, month, date, int(hours), int(minutes), int(seconds))
+                due_date = DEF_TZ.localize(due_date)
+            else:
+                due_date = datetime(year, month, date, 12, 0, 0)
+                due_date = DEF_TZ.localize(due_date)
+                
+            success = handler.set_deadline(task_id, chat_id, user_id, due_date)
+            
+            if not success:
+                update.message.reply_text('Вы не можете установить срок этому заданию.')
+            else:
+                update.message.reply_text('Срок выполнения установлен.')
+                
+        except (ValueError, ConnectionError):
+            update.message.reply_text('Извините, не получилось.')
+            return
     else:
-        update.message.reply_text('Срок выполнения установлен.')
+        update.message.reply_text('Извините, не получилось.')
 
 
 def get_list(update, context):
