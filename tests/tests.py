757baa0b25c28b1bab8d7c98f358f9a414fcd01a
Валентин Катышев <kutaxoff@icloud.com>
2019-05-23 16:44:48 +0300

    Add unit test filling all info fields

---
+++ tests/tests.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timezone
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+import db_connector
+
+
+class TaskCreateDestroyTest(TestCase):
+    def setUp(self):
+        self.db = db_connector.DataBaseConnector()
+        self.db._log = MagicMock()
+
+    def test_task_add_all_fields(self):
+        chat_id = 1
+        user_id = 1
+        task_text = 'Test task'
+        marked = True
+        deadline = datetime.now(timezone.utc)
+        workers = [1]
+        task_id = self.db.add_task(chat_id, user_id, task_text, marked,
+                                   deadline, workers)
+        self.assertIsInstance(task_id, int)
+        self.assertGreater(task_id, 0)
+        info = self.db.task_info(task_id)
+        self.assertEqual(task_id, info['id'])
+        self.assertEqual(user_id, info['creator_id'])
+        self.assertEqual(task_text, info['task_text'])
+        self.assertEqual(marked, info['marked'])
+        self.assertEqual(deadline, info['deadline'])
+        self.assertEqual(workers, info['workers'])
+
